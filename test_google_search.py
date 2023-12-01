b5f3bd1ecd65c09ec5d751118a095a4b89f3cf94
kglazova <kglazova@huntflow.ru>
2023-12-01 09:06:44 +0300

    задания 9_2, сделанное

--- test_google_search.py
+++ test_google_search.py
@@ -1,14 +1,21 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+@pytest.fixture
+def browser_size():
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+    yield
+    browser.quit()
 def test_google_search():
-    browser.open('https://www.google.ru/')
-    browser.element('#APjFqb').should(be.blank).type('yashaka/selen').press_enter()
-    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
 
 def test_google_search_not_found():
     browser.open('https://www.google.ru/')
-    browser.element('#APjFqb').should(be.blank).type('sfddfsfsfswedfwfewef').press_enter()
-    browser.element('#appbar').should(have.text('Результатов: примерно 0'))
+    browser.element('[name="q"]').should(be.blank).type('sfddfsfsfswedfwfewef').press_enter()
+    browser.element('[class="mnr-c"]').should(have.text('По запросу sfddfsfsfswedfwfewef ничего не найдено.'))
 
 
